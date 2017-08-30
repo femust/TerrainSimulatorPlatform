858fff8cb349e58212740bd405c25c92e2b77667
Andrzej Reinke <andreinkfemust@gmail.com>
2017-08-30 10:09:21 +0200

    wkng

---
+++ model_controller.cpp
@@ -0,0 +1,120 @@
+#include "model_controller.h"
+
+namespace gazebo
+{
+  GZ_REGISTER_MODEL_PLUGIN(ModelPush)
+
+   ModelPush::ModelPush()
+    {
+this->robotName="mobile_base";
+this->motorJointName01=robotName+"::"+"base_slider1";
+this->motorJointName02=robotName+"::"+"base_slider2";
+this->motorJointName03=robotName+"::"+"base_slider3";
+
+
+    }
+
+
+
+   ModelPush::~ModelPush()
+   {
+event::Events::DisconnectWorldUpdateBegin(this->updateConnection);
+   }
+
+    void ModelPush::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+    {
+
+      this->model = _parent; // Store the pointer to the model
+
+     this->motorJoint01=this->model->GetJoint(this->motorJointName01);
+      this->motorJoint02=this->model->GetJoint(this->motorJointName02);
+      this->motorJoint03=this->model->GetJoint(this->motorJointName03);
+
+
+
+     if (!this->motorJoint01 || !this->motorJoint02 || !this->motorJoint03)
+        {
+          gzerr << this->motorJoint01 << std::setw(10) << this->motorJoint02 << std::setw(10) << this->motorJoint03 << std::setw(10) << " some of them weren't found\n";
+          return;
+        }
+
+     this->activeJoints.push_back(motorJoint01);
+     this->activeJoints.push_back(motorJoint02);
+     this->activeJoints.push_back(motorJoint03);
+
+
+
+      this->vectorofJoints =this->model->GetJoints();
+
+       this->motorJoint01->SetAngle(0,0);
+     this->motorJoint02->SetAngle(0,0);
+      this->motorJoint03->SetAngle(0,0);
+
+
+
+
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(  // Listen to the update event. This event is broadcast every
+          boost::bind(&ModelPush::OnUpdate, this, _1));                  // simulation iteration.
+
+    }
+
+    // Called by the world update start event
+    void ModelPush::OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+
+      simTime = this->model->GetWorld()->GetSimTime();
+      realTime = this->model->GetWorld()->GetRealTime();
+
+     // std::cout<<"Sim Time: "<< simTime.Double()<<"Real Time: "<<realTime.Double() <<std::endl;
+
+
+      /*
+     for(physics::Joint_V::iterator jit=vectorofJoints.begin(); jit!=vectorofJoints.end(); ++jit)
+      {
+
+
+
+      std::cout<<"name:"<<std::setw(40)<<(*jit)->GetName()<<" "<<(*jit)->GetAngle(0)<<" "<<(*jit)->GetVelocity(0)<<" " << std::endl;
+
+     std::cout << "name: " << std::setw(40) << std::left << (*jit)->GetName() << "angle: " << std::setw(10) << std::left << (*jit)->GetAngle(0)<<"velocity: "<< std::setw(10) << std::left << (*jit)->GetVelocity(0)<<std::endl;// << setw(4) << hourlyRate << endl;
+
+        std::cout << "name: ";
+        std::cout<<(*jit)->GetName();
+        std::cout.width(40);
+        std::cout<<(*jit)->GetAngle(0);
+        std::cout.width(50);
+        std::cout<<(*jit)->GetVelocity(0)<<std::endl;
+
+       jointAngles[(*jit)->GetName()] = (*jit)->GetAngle(0).Radian();
+       jointVelocity[(*jit)->GetName()] = (*jit)->GetVelocity(0);
+       jointForce[(*jit)->GetName()] = (*jit)->GetForce(0);
+
+      }
+*/
+
+ this->motorJoint01->SetVelocity(1,0.01);
+       this->motorJoint02->SetVelocity(1,-0.01);
+
+     this->motorJoint03->SetVelocity(1,0.01);
+
+
+
+
+      for(physics::Joint_V::iterator jit=activeJoints.begin(); jit!=activeJoints.end(); ++jit)
+      {
+       std::cout << "name: " << std::setw(40) << std::left << (*jit)->GetName() << "angle: " << std::setw(15) << std::left << (*jit)->GetAngle(0)<<"velocity: "<< std::setw(15) << std::left << (*jit)->GetVelocity(0)<<std::endl;// << setw(4) << hourlyRate << endl;
+
+      }
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ model_controller.h
@@ -0,0 +1,60 @@
+#ifndef MODEL_PUSH_H
+#define MODEL_PUSH_H
+
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+#include <vector>
+#include<iostream>
+#include<iomanip>
+#include<iterator>
+#include<cmath>
+
+
+
+namespace gazebo
+{
+ class ModelPush : public ModelPlugin
+  {
+ public: ModelPush();
+  public: ~ModelPush();
+   public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
+   public: void OnUpdate(const common::UpdateInfo & /*_info*/); //virtual?
+
+
+   private: physics::ModelPtr model;
+
+   private: event::ConnectionPtr updateConnection;
+
+   private: physics::Joint_V vectorofJoints;
+   private: std::map<std::string, double> jointAngles;
+   private: std::map<std::string, double> jointVelocity;
+   private: std::map<std::string, double> jointForce;
+   private: physics::Joint_V  activeJoints;
+
+   private: common::Time simTime;
+   private: common::Time realTime;
+
+   private: std::string motorJointName01;
+   private: std::string motorJointName02;
+   private: std::string motorJointName03;
+   private : physics::JointPtr motorJoint01;
+   private: physics::JointPtr motorJoint02;
+   private: physics::JointPtr motorJoint03;
+
+    private: std::string robotName;
+  };
+
+ }
+
+
+
+
+
+
+
+
+
+#endif // MODEL_PUSH_H
